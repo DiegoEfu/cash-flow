2374250970a382a87871f1e08c91eba90aa7e2a1
Diego Faria <diegoafbrito12@gmail.com>
2024-06-24 17:06:48 -0400

    make login page and logout options

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', welcome_view, name="welcome_view")
+    path('', welcome_view, name="welcome_view"),
+    path('login', LoginView.as_view(), name="login_view"),
+    path('log-out', logout_view, name="logout_view")
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from typing import Any
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib.auth.views import  LoginView as BaseLoginView
+from django.contrib import messages
 
 # Create your views here.
 
 def welcome_view(request):
     return render(request, 'welcome.html')
+
+class LoginView(BaseLoginView):
+    template_name = 'login.html'
+
+    def get_success_url(self) -> str:
+        return '/'
+    
+    def get(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        if(request.user.is_authenticated):
+            return redirect('/')
+        
+        return super().get(request, *args, **kwargs)
+    
+    def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        response = super().post(request, *args, **kwargs)
+
+        if(response.status_code == 200 and not request.user.is_authenticated):
+            messages.warning(request, "Provided credentials are invalid.")
+
+        return response
+    
+def logout_view(request):
+    logout(request)
+    return redirect("/login")
