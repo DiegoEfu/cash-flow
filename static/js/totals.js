9aa4aa07021ef0f1a0a246e4bf5794bd5bb856af
Diego Faria <diegoafbrito12@gmail.com>
2024-07-16 14:21:38 -0400

    add previous values to make exact calculations without calling the server

--- static/js/totals.js
+++ static/js/totals.js
@@ -1,9 +1,6 @@
 document.querySelectorAll('.total').forEach(element => {
     element.addEventListener('keyup', totalKeyUp);
     element.addEventListener('change', totalChange);
-    element.addEventListener('focus', function() {
-        this.dataset.previousValue = this.value;
-    });
 });
 
 function totalKeyUp(event) {
@@ -16,60 +13,33 @@ function totalChange(event) {
 
 function calculateTotals(event) {
     let totalNow = 0;
-    let totalTags = 0;
     let totalAllTags = 0;
 
     const balance = parseFloat(document.getElementById('account_balance').value);
 
     document.querySelectorAll('.total').forEach(element => {
         let value = parseFloat(element.value);
+        const id = element.name.replace('-amount', '');
         if(value < 0){
             element.value = 0;
             value = 0;
         }
 
         totalNow += value;
+        const actual = parseFloat(document.getElementById(`total-tag-before-${id}`).innerText);
+        const previo = parseFloat(document.getElementById(`previous-${id}`).innerText);
+        const sum =  actual - previo + value;
+
+        document.getElementById(`total-tag-after-${id}`).innerText = isNaN(sum) ? 0 : sum.toFixed(2);
+        totalAllTags += sum;
     });
 
     if(isNaN(totalNow))
         totalNow = 0;
 
     document.getElementById('total-assigned-now').innerText = totalNow.toFixed(2);
+    document.getElementById('total-assigned-tags-after').innerText = totalAllTags.toFixed(2);
 
     let notAssigned = balance - totalNow;
     document.getElementById('not-assigned').innerText = notAssigned.toFixed(2);
-
-    document.querySelectorAll('.total-tag').forEach(element => {
-        let value = parseFloat(element.innerText);
-        if(value < 0){
-            element.value = 0;
-            value = 0;
-        }
-
-        totalTags += value;
-    });
-    document.getElementById('total-assigned-tags').innerText = totalTags.toFixed(2);
-
-    document.querySelectorAll('.total-tag').forEach(element => {
-        if(element.previousElementSibling === event.target) {
-            element.innerText = totalTags.toFixed(2);
-        }
-    });
-
-    document.querySelectorAll('.total-tag').forEach(element => {
-        let value = parseFloat(element.innerText);
-        if(value < 0){
-            element.value = 0;
-            value = 0;
-        }
-
-        totalAllTags += value;
-        if(element === event.target) {
-            totalAllTags -= parseFloat(element.dataset.previousValue);
-        }
-    });
-
-    document.getElementById('total-assigned-tags').innerText = totalAllTags.toFixed(2);
-    event.target.dataset.previousValue = event.target.value;
 }
-
